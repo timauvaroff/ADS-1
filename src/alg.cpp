4b8bb79da25d991619c9b74c439cc055e25bb186
Уваров Тимофей Максимович <66253074+timauvaroff@users.noreply.github.com>
2023-06-03 21:19:59 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,17 +4,51 @@
 
 
 bool checkPrime(uint64_t value) {
-// вставьте код функции
+  if (value < 2)
+          return false;
+      for (uint64_t i = 2; i <= std::sqrt(value); i++) {
+          if (value % i == 0)
+              return false;
+      }
+
+      return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-// вставьте код функции
+ uint64_t count = 0;
+    uint64_t num = 2;
+    
+    while (count < n) {
+        if (checkPrime(num)) {
+            count++;
+            if (count == n)
+                return num;
+        }
+        
+        num++;
+    }
+    
+    return 0;
 }
 
 uint64_t nextPrime(uint64_t value) {
-// вставьте код функции
+  uint64_t num = value + 1;
+
+      while (true) {
+          if (checkPrime(num))
+              return num;
+
+          num++;
+      }
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-// вставьте код функции
+  uint64_t sum = 0;
+
+      for (uint64_t i = 2; i < hbound; i++) {
+          if (checkPrime(i))
+              sum += i;
+      }
+
+      return sum;
 }
